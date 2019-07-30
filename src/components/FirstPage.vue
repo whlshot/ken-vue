661a6b40f003cc44464effa1a2b2ff17c5a596cc
whlshot <whlshot@163.com>
2019-07-30 22:40:02 +0800

    .

--- src/components/FirstPage.vue
+++ src/components/FirstPage.vue
@@ -1,7 +1,21 @@
 <template>
   <div>
     <headTop></headTop>
-    <h1>FirstPage</h1>
+    <section class="data-section">
+      <h2>数据统计</h2>
+      <el-row :gutter="40" class="section-one">
+        <el-col :span="4"><div class="data-list"><span>当日数据:22</span></div></el-col>
+        <el-col :span="4"><div class="data-list"><span>新增用户:1</span></div></el-col>
+        <el-col :span="4"><div class="data-list"><span>新增订单:7</span></div></el-col>
+        <el-col :span="4"><div class="data-list"><span>新增管理员:9</span></div></el-col>
+      </el-row>
+      <el-row :gutter="40" class="section-one">
+        <el-col :span="4"><div class="data-list"><span>总数据:100</span></div></el-col>
+        <el-col :span="4"><div class="data-list"><span>注册用户:20</span></div></el-col>
+        <el-col :span="4"><div class="data-list"><span>订单:60</span></div></el-col>
+        <el-col :span="4"><div class="data-list"><span>管理员:80</span></div></el-col>
+      </el-row>
+    </section>
   </div>
 </template>
 
@@ -17,5 +31,16 @@
 </script>
 
 <style scoped>
-
+  .data-section{
+    background-color: darkkhaki;
+    text-align: center;
+    padding: 20px;
+  }
+  .section-one{
+    margin: 20px;
+  }
+  .data-list{
+    background-color:yellowgreen;
+    border-radius: 6px;
+  }
 </style>
---
+++ src/components/Tendency.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="tendency">
+    <div id="tendency-chart">
+
+    </div>
+  </div>
+</template>
+
+<script>
+  import
+  export default {
+    name: "Tendency"
+  }
+</script>
+
+<style scoped>
+  .tendency {
+    display: flex;
+    justify-content: center;
+  }
+</style>
